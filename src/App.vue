3ad6be5a91d62d25b401de153026af9f1d05d5a3
Marcos García Yeh <antixsuperstar@gmail.com>
2019-03-30 18:32:37 -0600

    Release candidate 1? XD

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,13 @@
             <div class="col-md-8 offset-md-2">
                 <h3 class="text-center">Super quiz!</h3>
                 <hr />
-                <possible-guesses
-                        :numbers="numbers"
-                        :choices="choices"></possible-guesses>
-                <end-game></end-game>
+                <transition name="game" mode="out-in">
+                    <possible-guesses
+                            v-if="playing"
+                            :addends="addends"
+                            :choices="choices"></possible-guesses>
+                    <end-game v-else></end-game>
+                </transition>
             </div>
         </div>
     </div>
@@ -22,34 +25,63 @@
         name: 'app',
         data() {
             return {
-                range: 100,
-                numbers: [],
-                choices: [1,2,3],
-                playing: true
+                range: 100,     // Addends will be in the interval [-range, range]
+                addends: [],
+                sum: null,
+                choices: [],
+                playing: true,
+                max_addends: 2, // How many operands could the sum have?
+                max_choices: 6  // How many choices should I offer, in total?
             }
         },
         methods: {
             newPair() {
-                for (let i = 0; i<2; i++) {
-                    this.numbers[i] = Math.floor(Math.random() * this.range * Math.pow(-1, i));
+                // Note: I know about Array.forEach, but this echoes in my mind when I use it: https://hackernoon.com/3-javascript-performance-mistakes-you-should-stop-doing-ebf84b9de951
+                // (I can get a little obsessed sometimes about performance)
+
+                // Fill choices array with bogus answers in random position
+                this.choices = [];
+                for (let i=0; i<this.max_choices-1; i++) {
+                    this.choices.splice(Math.randInt(this.choices.length), 0, Math.randInt(this.range, -this.range));
                 }
-                this.logPairs();
-            },
-            logPairs() {
-                console.log(this.numbers);
+
+                // Create new sum
+                this.addends = [];
+                this.sum = 0;
+                for (let i=0; i<this.max_addends; i++) {
+                    this.addends[i] = Math.randInt(this.range) * Math.pow(-1, i);
+                    this.sum += this.addends[i];
+                }
+                // Insert good answer in random position
+                this.choices.splice(Math.randInt(this.choices.length), 0, this.sum);
+                //console.log(this.sum); // No cheating! :P
             }
         },
         created() {
-            this.newPair();
+            // I will use random integers a lot so let's create a shortcut here
+            Math.randInt = (max, min=0) => min + Math.ceil(Math.random() * max);
 
+            // I'll use this reference inside the eventBus object
+            const me = this;
+
+            // A choice button was clicked
             eventBus.$on('choose', guess => {
+                if (Number(guess) == this.sum) {
+                    this.playing = false;
+                } else {
+                    alert('Oops, wrong answer!\nCare to try again? :)');
+                }
+            });
 
-            });
+            // Start a new game
             eventBus.$on('newGame', function() {
-                console.log(this);
-                this.newPair();
-                this.playing = true;
+                // In here *this* points to eventBus not the App's Vue object
+                me.newPair();
+                me.playing = true;
             });
+
+            // At the beginning, start a new game :)
+            eventBus.$emit('newGame');
         },
         components: {
             possibleGuesses: PossibleGuesses,
@@ -67,4 +99,13 @@
   color: #2c3e50;
   margin-top: 60px;
 }
+    .game-enter-active, .game-leave-active {
+        transition: all 0.2s ease-in-out;
+    }
+    .game-enter-to, .game-leave {
+        transform: rotateY(0deg);
+    }
+    .game-enter, .game-leave-to {
+        transform: rotateY(90deg);
+    }
 </style>
--- src/components/PossibleGuesses.vue
+++ src/components/PossibleGuesses.vue
@@ -3,13 +3,16 @@
         <div class="col-12">
             <div class="card text-center">
                 <div class="card-header alert-secondary">
-                    <h4>What is {{ numbers[0] }} {{ numbers[1] }} ?</h4>
+                    <h4>What is <span v-for="(addend, index) in addends">{{ addend > 0 && index ? '+': '' }}{{ addend }}</span> ?</h4>
                 </div>
                 <div class="row">
-                    <div class="col-6 p-3" v-for="choice in choices">
+                    <div class="col-6 p-3" v-for="(choice, index) in choices">
+                        <!-- class attribute changes to include 'btn-primary' on hovering over a button -->
                         <button
-                                :class=""
-                                @mouseover="">{{ choice }}</button>
+                                :class="[ 'btn', index == highlightedChoice ? 'btn-primary' : 'btn-text' ]"
+                                @mouseover="highlightedChoice = index"
+                                @mouseout="highlightedChoice = null"
+                                @click="guessed(choice)">{{ choice }}</button>
                     </div>
                 </div>
             </div>
@@ -21,9 +24,17 @@
     import { eventBus } from '../main.js';
 
     export default {
+        data() {
+            return {
+                highlightedChoice: null
+            }
+        },
         props: {
-            numbers: Array,
+            addends: Array,
             choices: Array
+        },
+        methods: {
+            guessed: choice => { eventBus.$emit('choose', choice); }
         }
     }
 </script>
